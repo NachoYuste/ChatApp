bbf4575565275763cc363981cb9e7c713f260578
Nacho Yuste <ignacioyustel@live.com>
2024-01-29 23:24:20 +0100

    Add room management
    Create new topic for creating rooms and changing 'newMessage' topic to send message to a certain room

--- chat-socket/src/gateway/gateway.ts
+++ chat-socket/src/gateway/gateway.ts
@@ -1,26 +1,38 @@
-import { OnModuleInit } from "@nestjs/common";
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
-import { Server } from "socket.io";
+import { Body, OnModuleInit } from "@nestjs/common";
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Server, Socket } from "socket.io";
+import { Message } from "src/model/message";
 
 @WebSocketGateway()
-export class MyGateway implements OnModuleInit{
-    
+export class MyGateway implements OnModuleInit {
+
     onModuleInit() {
         this.server.on('connection', (socket) => {
             console.log(socket.id)
             console.log("Connected")
-        })    
+        })
     }
 
     @WebSocketServer()
     private readonly server: Server;
 
     @SubscribeMessage('newMessage')
-    onNewMessage(@MessageBody() body : any){
+    onNewMessage(@MessageBody() body: Message) {
         console.log(body);
-        this.server.emit('onMessage', {
-            msg: 'New Message',
-            content: body
+        this.server.to(body.room).emit('onMessage', {
+            msg: body.message
         })
+    }
+
+    @SubscribeMessage('joinRoom')
+    onJoinRoom(@MessageBody() room: any, @ConnectedSocket() socket: Socket) {
+        console.log(socket.id + " joining room: " + room);
+        socket.join(room);
+    }
+
+    @SubscribeMessage('leaveRoom')
+    onLeaveRoom(@MessageBody() room: any, @ConnectedSocket() socket: Socket) {
+        console.log(socket.id + ' leaving room: ' + room);
+        socket.leave(room);
     }
 }
