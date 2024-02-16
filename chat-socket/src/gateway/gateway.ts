f5d6a11da75b3ec713bb43db25e17781d107dc7e
Nacho Yuste <ignacioyustel@live.com>
2024-02-16 22:25:17 +0100

    Adapt gateway to new enum and finish room manage

--- chat-socket/src/gateway/gateway.ts
+++ chat-socket/src/gateway/gateway.ts
@@ -1,36 +1,44 @@
 import { Body, OnModuleInit } from "@nestjs/common";
 import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server, Socket } from "socket.io";
+import { GatewayEvents } from "src/model/gatewayEvents";
 import { Message } from "src/model/message";
 
-@WebSocketGateway()
+@WebSocketGateway(3001, {
+    cors:{
+        origin: '*',
+        methods: ['GET', 'POST'],
+        transports: ['websocket', 'polling'],
+        credentials: false,
+    },
+    allowEIO3: true
+})
 export class MyGateway implements OnModuleInit {
-
-    onModuleInit() {
-        this.server.on('connection', (socket) => {
-            console.log(socket.id)
-            console.log("Connected")
-        })
-    }
 
     @WebSocketServer()
     private readonly server: Server;
 
-    @SubscribeMessage('newMessage')
+    onModuleInit() {
+        this.server.on('connection', (socket) => {
+            console.log("Connected: " + socket.id);
+        });
+    }
+
+    @SubscribeMessage(GatewayEvents.ReceiveMessage)
     onNewMessage(@MessageBody() body: Message) {
         console.log(body);
-        this.server.to(body.room).emit('onMessage', {
+        this.server.to(body.room).emit(GatewayEvents.EmitMessage, {
             msg: body.message
         })
     }
 
-    @SubscribeMessage('joinRoom')
+    @SubscribeMessage(GatewayEvents.JoinRoom)
     onJoinRoom(@MessageBody() room: any, @ConnectedSocket() socket: Socket) {
         console.log(socket.id + " joining room: " + room);
         socket.join(room);
     }
 
-    @SubscribeMessage('leaveRoom')
+    @SubscribeMessage(GatewayEvents.LeaveRoom)
     onLeaveRoom(@MessageBody() room: any, @ConnectedSocket() socket: Socket) {
         console.log(socket.id + ' leaving room: ' + room);
         socket.leave(room);
